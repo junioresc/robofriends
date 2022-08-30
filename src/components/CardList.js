b804b22228aa738f73a3f69e99b3f2cbfb06ecd8
Junior Escobar <junioresc1092@gmail.com>
2022-08-29 21:44:54 -0400

    created cardList component that takes the robot information and loops over each user creating a card

---
+++ src/components/CardList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from './Card';
+
+function CardList({ robots }) {
+    return (
+        <div className='cardlist'>
+            {
+                robots.map((user, i) => {
+                    return (
+                        <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default CardList;
